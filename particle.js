d3c4f409dd86ef8a71cf7652e05363106eb1036d
guisopo <solerguillermo@hotmail.com>
2018-11-30 12:44:37 +0100

    Added methods and properties to simulate gravity between particles

--- particle.js
+++ particle.js
@@ -2,6 +2,9 @@ var particle = {
   position: null,
   velocity: null,
   gravity: null,
+  mass: 1,
+  radius: 0,
+  bounce: -1,
 
   create: function(x, y, speed, direction, grav) {
     var obj = Object.create(this);
@@ -20,6 +23,26 @@ var particle = {
   update: function() {
     this.velocity.addTo(this.gravity);
     this.position.addTo(this.velocity);
+  },
+
+  angleTo:  function(p2) {
+    return Math.atan2(p2.position.getY() - this.getY(), p2.position.getX() - this.getX());
+  },
+
+  distanceTo: function(p2) {
+    var dx = p2.position.getX() - this.getX();
+    var dy = p2.position.getY() - this.getY();
+    return Math.sqrt(dx * dx + dy * dy);
+  },
+
+  gravitateTo: function(p2) {
+    var grav = vector.create(0, 0),
+        dist = this.distanceTo(p2);
+
+    grav.setLength(p2.mass / (dist * dist));
+    grav.setAngle(this.angleTo(p2));
+
+    this.vector.addTo(grav);
   }
 
 }
