952788c2b2ab045138a252b8a1a2ba0fa5978348
guisopo <solerguillermo@hotmail.com>
2018-11-30 10:25:43 +0100

    Created Vector Object with basics Methods

---
+++ vector.js
@@ -0,0 +1,41 @@
+var vector = {
+  _x: 1,
+  _y: 0,
+
+  setX: function(value) {
+    this._x = value;
+  },
+
+  getX: function() {
+    return this._x;
+  },
+
+  setY: function(value) {
+    this._y = value;
+  },
+
+  getY: function() {
+    return this._y;
+  },
+
+  setAngle: function(angle) {
+    var length = this.getLength();
+    this._x = Math.cos(angle) * length;
+    this._y = Math.sin(angle) * length;
+  },
+
+  getAngle: function() {
+    return Math.atan2(this._y, this._x);
+  },
+
+  setLength: function(length) {
+    var angle = this.getAngle();
+    this._x = Math.cos(angle) * length;
+    this._y = Math.sin(angle) * length;
+  },
+
+  getLength: function() {
+    return Math.sqrt( this._x * this._x + this._y * this._y);
+  }
+
+}
