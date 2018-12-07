dbeda4ae0957ff4144b1cca7963c9d06bb45439b
guisopo <solerguillermo@hotmail.com>
2018-12-07 14:23:15 +0100

    Created new spiral example

---
+++ examples/2d-spiral.js
@@ -0,0 +1,61 @@
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth,
+		height = canvas.height = window.innerHeight;
+		focalLength = 300,
+		particles = [],
+		numberOfParticles = 200,
+		centerZ = 2000,
+		radius = 1000,
+		baseAngle = 0,
+		rotationSpeed = 0.01;
+
+	for(var i = 0; i < numberOfParticles; i++) {
+		var particle = {
+			angle: 0.2 * i,
+			y: 2000 - 4000 / numberOfParticles * i
+		};
+		particle.x = Math.cos(particle.angle + baseAngle) * radius;
+		particle.z = centerZ + Math.sin(particle.angle + baseAngle) * radius;
+		particles.push(particle);
+	}	
+
+	context.translate(width/2, height/2);
+
+	document.addEventListener('mousemove', function(event) {
+		rotationSpeed = (event.clientX - width/2) * 0.00005;
+		yPos = (event.clientY - height / 2) * 2;
+	});
+
+	update();
+	function update() {
+		baseAngle += rotationSpeed;
+		context.clearRect(-width/2, -height/2, width, height);
+
+		particles.forEach(particle => {
+			var perspective = focalLength / (focalLength + particle.z);
+			context.save();
+			context.scale(perspective, perspective);
+			context.translate(particle.x, particle.y);
+
+			context.beginPath();
+			context.arc(0, 0, 20, 0, Math.PI * 2, false);
+			context.fill();
+			
+			context.restore();
+			
+			particle.x = Math.cos(particle.angle + baseAngle) * radius;
+			particle.z = centerZ + Math.sin(particle.angle + baseAngle) * radius;
+			
+		});
+		
+		requestAnimationFrame(update);
+	}
+
+	function zSort(cardA, cardB) {
+		return cardB.z - cardA.z;
+	}
+
+	
+};
