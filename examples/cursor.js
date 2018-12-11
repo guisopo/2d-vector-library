ce6f4f6fa049e66f45b7b96a1fb80d111f499370
guisopo <solerguillermo@hotmail.com>
2018-12-11 17:40:18 +0100

    Updated example

--- examples/cursor.js
+++ examples/cursor.js
@@ -1,62 +1,64 @@
+// TO DO
+// Update functionality to be able to add any number of weights ('followers')
+// Create canvas class with resize event listener
+
 window.onload = function() {
-  var canvas = document.getElementById('canvas'),
-      context = canvas.getContext('2d'),
-      width = canvas.width = window.innerWidth,
+	var canvas = document.getElementById("canvas"),
+			context = canvas.getContext("2d"),
+			width = canvas.width = window.innerWidth,
       height = canvas.height = window.innerHeight,
 
-      springPoint = vector.create(width/2, height/2),
-      weight = particle.create(Math.random() * width, Math.random() * height, 0, 0),
-      
-      k = 0.1,
+      springPoint = new Particle(Math.random() * width, Math.random() * height, 0, 0),
+      weight = new Particle(Math.random() * width, Math.random() * height, 0, 0),
+      weight2 = new Particle(Math.random() * width, Math.random() * height, 0, 0),
+
+      k = 1,
+      springLength = 0,
       minRadius = 12,
       maxRadius = 20;
 
   weight.radius = minRadius;
-  weight.friction = 0.67;
+  weight.friction = 0.1;
+  weight2.friction = 0.06;
+  weight.addSpring(springPoint, k, springLength);
+  weight2.addSpring(weight, k, springLength);
+
   
   document.addEventListener('mousemove', function(event) {
-    springPoint.setX(event.clientX);
-    springPoint.setY(event.clientY);
+    springPoint.x = event.x;
+    springPoint.y = event.y;
+    
   });
-  
-  update();
 
-  function update() {
+	update();
+
+	function update() {
     context.clearRect(0, 0, width, height);
     
-    context.globalCompositeOperation = 'xor';
-
-    // Spring Effect Opertations
-    var distance = springPoint.subtract(weight.position),
-        springForce = distance.multiply(k);
-
-    weight.velocity.addTo(springForce);
-
-    // Mapping Weight Radius to it's Velocity 
-    weight.radius = utils.clamp(weight.velocity.getLength(), minRadius, maxRadius);
-
+    weight.radius = utils.clamp(weight.getSpeed(), minRadius, maxRadius);
+    
     weight.update();
-
-
-    // Spring Point (Pointer)
+    weight2.update();
+    
+    // Weight 2 (Follower)
     context.beginPath();
     context.fillStyle = 'black';
-    context.arc(springPoint.getX(), springPoint.getY(), 6, 0, Math.PI * 2, false);
+    context.arc(weight.x, weight.y, weight.radius, 0, Math.PI * 2, false);
     context.fill();
-
-    // Weight (Follower)
-    context.beginPath();
-    context.fillStyle = 'black';
-    context.arc(weight.position.getX(), weight.position.getY(), weight.radius, 0, Math.PI * 2, false);
+    // Weight 2 (Follower 2)
+    context.globalCompositeOperation = 'difference';
+		context.beginPath();
+    context.arc(weight2.x, weight2.y, weight.radius, 0, Math.PI * 2, false);
     context.fill();
     
-    // Line between Spring Point and Weight
-    // context.beginPath();
-    // context.lineWidth = 1;
-    // context.moveTo(weight.position.getX(), weight.position.getY());
-    // context.lineTo(springPoint.getX(), springPoint.getY());
-    // context.stroke();
-
+    // Spring Point (Pointer)
+    context.globalCompositeOperation = 'xor';
+    context.beginPath();
+    context.fillStyle = 'black';
+    context.arc(springPoint.x, springPoint.y, 6, 0, Math.PI * 2, false);
+    context.fill();
     requestAnimationFrame(update);
-  }
+	}
+
+
 };
