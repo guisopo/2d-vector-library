1111fad392ebc500156a5da70ccdc97ae80cb431
guisopo <solerguillermo@hotmail.com>
2018-12-17 16:46:51 +0100

    Improved relation between weight speed and weight radius and commented weight 2

--- examples/cursor.js
+++ examples/cursor.js
@@ -10,24 +10,23 @@ window.onload = function() {
 
       springPoint = new Particle(Math.random() * width, Math.random() * height, 0, 0),
       weight = new Particle(Math.random() * width, Math.random() * height, 0, 0),
-      weight2 = new Particle(Math.random() * width, Math.random() * height, 0, 0),
+      // weight2 = new Particle(Math.random() * width, Math.random() * height, 0, 0),
 
       k = 1,
       springLength = 0,
-      minRadius = 12,
-      maxRadius = 20;
+      minRadius = 25,
+      maxRadius = 60;
 
   weight.radius = minRadius;
   weight.friction = 0.1;
-  weight2.friction = 0.06;
+  // weight2.friction = 0.06;
   weight.addSpring(springPoint, k, springLength);
-  weight2.addSpring(weight, k, springLength);
+  // weight2.addSpring(weight, k, springLength);
 
   
   document.addEventListener('mousemove', function(event) {
     springPoint.x = event.x;
     springPoint.y = event.y;
-    
   });
 
 	update();
@@ -35,10 +34,11 @@ window.onload = function() {
 	function update() {
     context.clearRect(0, 0, width, height);
     
-    weight.radius = utils.clamp(weight.getSpeed(), minRadius, maxRadius);
-    
+    speed = weight.getSpeed();
+
+    weight.radius = utils.map(speed, 0, 90, minRadius, maxRadius);
     weight.update();
-    weight2.update();
+    // weight2.update();
     
     // Weight 2 (Follower)
     context.beginPath();
@@ -46,16 +46,16 @@ window.onload = function() {
     context.arc(weight.x, weight.y, weight.radius, 0, Math.PI * 2, false);
     context.fill();
     // Weight 2 (Follower 2)
-    context.globalCompositeOperation = 'difference';
-		context.beginPath();
-    context.arc(weight2.x, weight2.y, weight.radius, 0, Math.PI * 2, false);
-    context.fill();
+    // context.globalCompositeOperation = 'difference';
+		// context.beginPath();
+    // context.arc(weight2.x, weight2.y, weight.radius, 0, Math.PI * 2, false);
+    // context.fill();
     
     // Spring Point (Pointer)
     context.globalCompositeOperation = 'xor';
     context.beginPath();
     context.fillStyle = 'black';
-    context.arc(springPoint.x, springPoint.y, 6, 0, Math.PI * 2, false);
+    context.arc(springPoint.x, springPoint.y, 5, 0, Math.PI * 2, false);
     context.fill();
     requestAnimationFrame(update);
 	}
