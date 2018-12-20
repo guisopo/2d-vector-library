212126816a4156ddc40372d6fb7bfdbf0ded9226
guisopo <solerguillermo@hotmail.com>
2018-12-20 16:56:49 +0100

    Created new example

---
+++ examples/particle-grid.js
@@ -0,0 +1,40 @@
+// TO DO
+// Update functionality to be able to add any number of weights ('followers')
+// Create canvas class with resize event listener
+
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+			context = canvas.getContext("2d"),
+			width = canvas.width = window.innerWidth,
+      height = canvas.height = window.innerHeight,
+      particlesH = 5,
+      particlesV = 5,
+      particles = [];
+
+  console.log(width/20, height/10);
+
+  for (let i = 0; i <= particlesH; i++) {
+    for (let j = 0; j <= particlesV; j++) {
+      p = new Particle (width/ i, height/ j, 0, 0);
+      particles.push(p);
+    }
+  }
+
+
+	update();
+
+	function update() {
+    context.clearRect(0, 0, width, height);
+
+    particles.forEach(particle => {
+      context.beginPath();
+      context.arc(particle.x, particle.y.y, 5, 0, Math.PI * 2, false);
+      context.fill();
+
+    })
+    
+    requestAnimationFrame(update);
+	}
+
+
+};
