24683b3e580b0d2ad8d58d8c174f7a057a833904
guisopo <solerguillermo@hotmail.com>
2018-12-21 23:47:58 +0100

    Created second canvas to create second grid over the first one. To do: Check performace

--- examples/particle-grid.js
+++ examples/particle-grid.js
@@ -3,17 +3,25 @@ window.onload = function() {
 			context = canvas.getContext("2d"),
 			width = canvas.width = window.innerWidth,
       height = canvas.height = window.innerHeight,
+      
+      canvas2 = document.getElementById("canvas2"),
+			context2 = canvas2.getContext("2d");
+			canvas2.width = width;
+      canvas2.height = height;
+
 
     // Number of particles
-      particlesH = 100,
-      particlesV = 100,
+    const  particlesH = 30,
+           particlesV = 30,
 
     // Margin between particles
       marginH = width / particlesH,
       marginV = height / particlesV,
 
       particles = [],
+      particles2 = [],
       target = new Particle(0, 0, 0, 0);
+
     let   targetK = -0.4,
           springDistance = 60;
 
@@ -30,6 +38,17 @@ window.onload = function() {
     }
   }
 
+  for (let i = 0; i < particlesH; i++) {
+    for (let j = 0; j < particlesV; j++) {
+      p = new Particle (i * marginH , j * marginV , 0, 0);
+
+      p.friction = 0.9;
+      p.setSpringTarget(p.x, p.y, 0.02);
+      
+      particles2.push(p);
+      
+    }
+  }
   document.body.addEventListener('mousemove', function(event) {
     target.x = event.clientX;
     target.y = event.clientY;
@@ -49,13 +68,23 @@ window.onload = function() {
     context.clearRect(0, 0, width, height);
 
     particles.forEach(particle => {
-      // particle.springFrom(particle, 0.02);
+      particle.update();
       particle.springFrom(target, targetK, springDistance);
       
-      particle.update();
       context.beginPath();
-      context.arc(particle.x, particle.y, 1, 0, Math.PI * 2, false);
+      context.arc(particle.x, particle.y, 1.5, 0, Math.PI * 2, false);
       context.fill();
+    })
+    
+    context2.clearRect(0, 0, width, height);
+
+    particles2.forEach(particle => {
+      particle.update();
+      particle.springFrom(target, targetK, springDistance);
+
+      context2.beginPath();
+      context2.arc(particle.x, particle.y, 1.5, 0, Math.PI * 2, false);
+      context2.fill();
     })
     
     requestAnimationFrame(update);
