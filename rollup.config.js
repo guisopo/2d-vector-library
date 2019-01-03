0b9fb89a8647bcea58adbb1f9581b1590f88b4d8
guisopo <solerguillermo@hotmail.com>
2019-01-03 20:19:20 +0100

    Added plugin replace for versioning and rimraf to delete dist files

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,21 @@
+import replace from 'rollup-plugin-replace';
+import rimraf from 'rimraf';
+
+rimraf.sync('dist');
+
+const pkg = require('./package.json');
+const version = pkg.version;
+
 export default {
   input: 'src/scripts/main.js',
   output: {
     file: 'dist/scripts/main.js',
     format: 'iife'
-  }
+  },
+  plugins: [
+    replace({
+      delimiters: ['{{', '}}'],
+      version
+    })
+  ]
 }
