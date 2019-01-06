a60d109a3555b8133795334c0341d6752b613972
guisopo <solerguillermo@hotmail.com>
2019-01-06 19:54:13 +0100

    Calculate size of the canvas and adjust it based on the user’s device pixel ratio, or pixel density. This ensures that the final result is crisp on all monitors.

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -2,21 +2,24 @@ export class Canvas {
   constructor() {
     this.canvas = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
-
+    this.dpr = window.devicePixelRatio;
+    
     document.body.appendChild(this.canvas);
-
+    
     window.addEventListener('resize', () => this.onResize());
     this.onResize();
-
+    
     // this.updateBound = this.update.bind(this);
     // requestAnimationFrame(this.updateBound);
   }
-
+  
   onResize() {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
-    this.canvas.width = this.width;
-    this.canvas.height = this.height;
+    this.canvas.width = this.width * this.dpr;
+    this.canvas.height = this.height * this.dpr;
+
+    this.context.scale(this.dpr, this.dpr);
   }
 
   // update() {
