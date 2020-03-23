33d5100625dab5047af8d299e3fc5e56a90cf392
guisopo <solerguillermo@hotmail.com>
2020-03-23 19:50:22 +0100

    Added lerp function

--- formulas.js
+++ formulas.js
@@ -2,3 +2,8 @@ function norm(value, min, max) {
   // Takes a value from a range and returns a value from 0 to 1
   return value - min / max - min;
 }
+
+function lerp(norm, min, max) {
+  // Takes a normalized value and returns the value within a range
+  return (max - min) * norm + min;
+}
